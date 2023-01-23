209ddf5d552200f539c961952198550e6c91e91b
Thiago L <thiago.tenorio07@gmail.com>
2023-01-23 09:19:36 -0300

    spring mongo

---
+++ src/test/java/com/othiagotenorio/startmongodb/StartmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.othiagotenorio.startmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
